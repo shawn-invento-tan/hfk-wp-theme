86f1f9c9eeab4aa6281442c646f717429fa54afc
shawn-invento-tan <36098763+shawn-invento-tan@users.noreply.github.com>
2019-02-09 11:33:55 +0800

    Template uploads and styling changes

--- page-programs.php
+++ page-programs.php
@@ -3,16 +3,22 @@
 ?>
 <div class="content-container py-5">
     <h1 class="text-bold text-center mb-4">Programs</h1>
-    <div class="row">
+    <div class="row text-decoration-none justify-content-center">
         <div class="col-lg-4 col-md-4 col-sm-6 col-12 col-bottom-fix">
-            <div class="card border-0">
-                <div class="card-img-top">
+            <div class="card description-card">
+                <div class="card-img-top border rounded-0">
                     <div class="aspect-ratio-16-9 bg-dark">
+                        <img src="/hfkui/wp-content/themes/hfk-wp-theme/assets/property-form-preview.png">
                     </div>
                 </div>
-                <div class="card-body px-0">
-                    <h5 class="text-bold">Test Program 1</h5>
-                    <p>This program does so and so and so and so</p>
+                <div class="card-body">
+                    <h5 class="text-bold">
+                        <a href="#" target="_blank" download class="link-black">
+                            Property Form
+                        </a>
+                    </h5>
+                    <p>This form is for venue bookings by ministries, zones or BECs for parish related activities</p>
+                    <p><a href="#" download>Download the form</a>. Fill up required information. Scan and email form to hfckajang@gmail.com for approval</p>
                 </div>
             </div>
         </div>
